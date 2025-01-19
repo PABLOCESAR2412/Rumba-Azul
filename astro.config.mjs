20056aa36b8cbb78bf1535c08185f73ededadb59
PABLOCESAR2412 <pablocesar.t.g@gmail.com>
2025-01-19 00:24:38 -0500

    Version 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import vercel from '@astrojs/vercel';
-
 import tailwind from '@astrojs/tailwind';
+import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
-  integrations: [tailwind()]
+    adapter: vercel(),
+    integrations: [tailwind(), alpinejs()]
 });
